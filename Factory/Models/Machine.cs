f88fc3b14e278015f42eabeee54982481b020444
Brishna Bakshev <b.bakshev@gmail.com>
2023-03-17 08:56:03 -0700

    completed machine modes code

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+    public class Machine
+    {
+      public int MachineId { get; set; }
+
+      [Required (ErrorMessage = "Please enter name of the machine.")]
+      public string Name { get; set; }
+      public DateTime InspectionDate { get; set; }
+      public List<EngineerMachine> JoinEntities { get; set; }
+    }
+}
