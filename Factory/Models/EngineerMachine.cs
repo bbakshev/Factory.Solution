1949a63da98ab33c67e52ed1d9cbd6d5b012f423
Brishna Bakshev <b.bakshev@gmail.com>
2023-03-17 09:05:50 -0700

    updates to add error message

--- Factory/Models/EngineerMachine.cs
+++ Factory/Models/EngineerMachine.cs
@@ -3,7 +3,11 @@ namespace Factory.Models
     public class EngineerMachine
     {
         public int EngineerMachineId { get; set; }
+        
+        [Range(1, int.MaxValue, ErrorMessage = "Please select an engineer.")]
         public int EngineerId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a machine.")]
         public int MachineId { get; set; }
         public Engineer Engineer { get; set; }
         public Machine Machine { get; set; }
